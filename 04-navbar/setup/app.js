a5a21627ae1b481f02dc5bf87cefcb41222a4879
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-03 13:32:36 +0300

    finished navbar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,12 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const toggleBtn = document.getElementById("toggle-content");
+
+toggleBtn.addEventListener("click", function () {
+  console.log("btn clicked");
+  const ul = document.querySelector("ul");
+  console.log(ul.classList.toggle("toggle-content"));
+});
+console.log(toggleBtn);
